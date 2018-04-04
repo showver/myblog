abc7e2a60d2d321f5e75d20512040ed3e33beb75
showver <showver@sohu.com>
2018-04-04 23:51:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<?php
-//确定目录
-define("APP_NAME",'Home');
-//确定目录路径
-define("APP_PATH",'./Home/');
-//应用文件ThinkPHP.php
-require './ThinkPHP/ThinkPHP.php';
-//开启调试模式
-define("APP_DEBUG",true);
-
-?>
+test
